f8e81929d90c4bc1c9d45305a59e921b679ad8dd
Abhinav Kumar <kumaraka2000@gmail.com>
2023-04-18 18:23:04 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Solution solution = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        for(int e : solution.twoSum(nums, target))
+        {
+            System.out.println(e);
+        }
     }
 }
